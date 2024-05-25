34563e6c741d266bd54f3e4a90ffb0f81b691293
Dmitriy <goglev37@gmail.com>
2024-05-25 13:23:22 +0300

    задания 1 урока

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ console.log(originalArray);*/
 
 /*const user = {name: "John", age: 25, city: "New York"};
 
-let {name, age, city} = {...user}
+let {name, age, city} = user
 
 console.log(name);  // 'John'
 console.log(age);   // 25
@@ -45,7 +45,7 @@ console.log(city);  // 'New York'*/
 // и вывести в консоль эти переменные
 
 /*const fruits = ["apple", "banana", "orange"];
-const [, secondFruit, thirdFruit] = [...fruits]
+const [, secondFruit, thirdFruit] = fruits
 
 console.log(secondFruit);  // 'banana'
 console.log(thirdFruit);   // 'orange'*/
@@ -53,12 +53,50 @@ console.log(thirdFruit);   // 'orange'*/
 
 // Использовать деструктуризацию в параметрах функции
 
-const person = {name: "Alice", age: 30};
+/*const person = {name: "Alice", age: 30};
 
 // Деструктуризация в параметрах функции
-function printPersonDetails() {
-
+function printPersonDetails({name,age}) {
     console.log(`Name: ${name}, Age: ${age}`);
-}
+}*/
 
-printPersonDetails(person);  // Вывод: Name: Alice, Age: 30
+// printPersonDetails(person);  // Вывод: Name: Alice, Age: 30
+
+// Установите значение по умолчанию для свойства year которого нет в объекте car
+
+// const car = {brand: "Toyota", model: "Camry"};
+
+// выведите в консоль его
+/*const {brand, model, year = "2022"} = car
+
+console.log(brand);  // 'Toyota'
+console.log(model);  // 'Camry'
+console.log(year);   // 2022 (значение по умолчанию)*/
+
+// Извлечь значения из массива объектов
+/*const students = [
+    {name: "Alex", grade: "A"},
+    {name: "Emma", grade: "B"},
+    {name: "Chris", grade: "C"},
+];*/
+/*const student1 = students[0].name
+const student3 = students[2].name*/
+/*const [student1, , student3] = students.map(e => e.name)
+
+console.log(student1);  // 'Alex'
+console.log(student3);  // 'Chris*/
+
+
+// Установите значение по умолчанию для свойства age которого нет в объекте person
+// и переименовать переменные, чтобы достать значение с свойства firstName
+/*const person = {firstName: "Max", lastName: "Johnson"};
+
+const firstName = "first Name: Nina";
+const lastName = "last Name: Ivanov";
+
+const {firstName: myName, lastName: secondName, age = "25"} = person*/
+// и вывести результат в консоль
+/*
+console.log(myName); // 'Max'
+console.log(secondName); // 'Johnson'
+console.log(age); // 25 (значение по умолчанию)*/
